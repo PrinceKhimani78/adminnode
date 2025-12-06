5bca69c2757997639c56dd0eee6d47c42e460d5a
PrinceKhimani78 <prince@mutanttechnololiges.com>
2025-12-06 20:27:44 +0530

    Fix start.sh path for demo standalone deploy

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/demo.rojgariindia.com/app
+cd /home/demo.rojgariindia.com/app/release
 echo ">>> Starting Demo Next.js on port 3020"
-node server.js
+npm run start -- -p 3020 -H 0.0.0.0

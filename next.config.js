4eb3b347cedbc6be9e3a6f153f47c6008f7198aa
Princekhimani78 <prince@mutanttechnologies.com>
2025-12-12 13:26:36 +0530

    aw

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "randomuser.me" },
+      { protocol: "https", hostname: "1000logos.net" },
+      { protocol: "https", hostname: "logos-world.net" },
+      { protocol: "https", hostname: "imgcdn.stablediffusionweb.com" },
+      { protocol: "https", hostname: "static.vecteezy.com" },
+      { protocol: "https", hostname: "cdn-icons-png.flaticon.com" },
+      { protocol: "https", hostname: "seeklogo.com" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+    ],
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  swcMinify: true,
+};
+
+module.exports = nextConfig;

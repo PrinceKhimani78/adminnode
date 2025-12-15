c873dd7f0bee630d12c04508b23ae261e1b1e6a4
Princekhimani78 <prince@mutanttechnologies.com>
2025-12-15 14:28:19 +0530

    aa

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -e
 
 export NODE_ENV=production
 export PORT=3020
 
 cd /home/rojgariindia.com/app
 
-echo ">>> Starting rojgariindia.com on port 3020"
+echo ">>> Starting rojgariindia.com (custom Next.js server) on port 3020"
 
 exec node server.js

a7263a5061f5fa8897230bbafd552c33ba27ea70
Princekhimani78 <prince@mutanttechnologies.com>
2025-12-18 19:31:55 +0530

    aw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Home from "../components/Home/Home";
 
 export const metadata = {
-  title: "Home | Rojgari-India",
+  title: "Home |Backend Rojgari-India",
   description: "Welcome to the homepage of my Next.js app!",
 };
 
